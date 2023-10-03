4046a46b660277a0f2c0890c0646d9801540fb17
Ana Isabel Carvalho <a@manufacturaindependente.org>
2023-10-03 13:22:54 +0100

    Update menu layout; compact menu

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,40 +1,39 @@
 <template>
-  <v-system-bar color="background">
-    <v-menu>
-      <template v-slot:activator="{ props }">
-        <v-btn color="primary" v-bind="props" variant="plain" density="compact">
-          {{ locale }}
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item v-for="lang in availableLocales" :key="lang" @click="locale = lang">
-          <v-list-item-title>{{ lang }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
-  </v-system-bar>
   <v-app-bar
     color="background"
     elevation="0"
-    class="pa-2"
-    density="prominent"
+    class="pa-2 my-4"
+    density="compact"
     scroll-behavior="hide elevate"
     height="60"
     tag="nav"
   >
-    <div class="d-flex align-center h-100 pl-4">
-      <v-img
-        src="/images/logo-horizontal-black.png"
-        height="60"
-        width="209"
-        alt="Faktenforum Logo"
-      />
-    </div>
-
+    <v-app-bar-title>
+      <div class="h-100">
+        <v-img
+          src="/images/logo-horizontal-black.png"
+          height="60"
+          width="209"
+          alt="Faktenforum Logo"
+        />
+      </div>
+    </v-app-bar-title>
     <template v-slot:append>
-      <v-btn color="primary" class="px-6" variant="text">What is Faktenforum</v-btn>
-      <v-btn color="primary" class="px-6" variant="text">News</v-btn>
-      <v-btn color="primary" class="pe-10 mr-6" variant="text">FAQ</v-btn>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn color="text-color" class="px-6 text-uppercase" v-bind="props" variant="text">
+            {{ locale }}
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="lang in availableLocales" :key="lang" @click="locale = lang">
+            <v-list-item-title>{{ lang }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-btn color="text-color" class="px-4" variant="text">What is Faktenforum</v-btn>
+      <v-btn color="text-color" class="px-4" variant="text">News</v-btn>
+      <v-btn color="text-color" class="px-4" variant="text">FAQ</v-btn>
       <NuxtLink to="/join">
         <v-btn color="primary" class="px-10 text-color-secondary" variant="flat">Join</v-btn>
       </NuxtLink>
